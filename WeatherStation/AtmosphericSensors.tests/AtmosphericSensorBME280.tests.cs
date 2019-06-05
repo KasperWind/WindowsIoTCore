9b55eccffb1d3479551b2053db958b0225ca9bbf
Kasper Wind <Kasper@Wind.as>
2019-06-05 21:50:05 +0200

    Added tests project

---
+++ WeatherStation/AtmosphericSensors.tests/AtmosphericSensorBME280.tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using AtmosphericSensors;
+using AtmosphericSensors.HardwareIO;
+
+namespace AtmosphericSensors.tests
+{
+    public class AtmosphericSensorBME280_tests
+    {
+        [Fact]
+        public void DoSomeTest()
+        {
+            var test = new AtmosphericSensorBME280(new I2cSensor(0x77));
+            
+        }
+    }
+}
