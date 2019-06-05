896db783b2599393582da848b44ea0e10fb45fbb
Kasper Wind <Kasper@Wind.as>
2019-06-05 21:56:53 +0200

    Added test in Compensations for throws at invalid creation

--- WeatherStation/AtmosphericSensors.tests/AtmosphericSensorBME280Compensations.testd.cs
+++ WeatherStation/AtmosphericSensors.tests/AtmosphericSensorBME280Compensations.testd.cs
@@ -7,6 +7,12 @@ namespace AtmosphericSensors.tests
 {
     public class AtmosphericSensorBME280Compensations_tests
     {
-
+        [Fact]
+        public void ThrowsWhenCreatedWithInvalidCensor()
+        {
+            Assert.Throws<Exception>(
+                () => new AtmosphericSensorBME280Compensations(new I2cSensor(0))
+                );
+        }
     }
 }
