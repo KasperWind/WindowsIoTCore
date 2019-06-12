289d95a61e63e17e63c913e4cfc1f1803a3347c3
Kasper Wind <Kasper@Wind.as>
2019-06-12 22:29:40 +0200

    Added write to II2cSensor interface

--- WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
+++ WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
@@ -8,5 +8,7 @@ namespace AtmosphericSensors.HardwareIO
         UInt16 ReadUInt16Register(byte address);
         Int16 ReadInt16Register(byte address);
         Int32 Read20bitRegister(byte address);
+
+        void WriteRegister(byte address, byte[] data);
     }
 }
