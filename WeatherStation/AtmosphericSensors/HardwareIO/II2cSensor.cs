6b736656959de6b86d676a9172489bedd1d8fdcf
Kasper Wind <Kasper@Wind.as>
2019-06-12 21:39:44 +0200

    refactored naming in interface II2cSensor

--- WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
+++ WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
@@ -4,7 +4,7 @@ namespace AtmosphericSensors.HardwareIO
 {
     public interface II2cSensor
     {
-        byte ReadRegister(byte registerAddress);
+        byte ReadRegister(byte address);
         UInt16 ReadUInt16Register(byte address);
         Int16 ReadInt16Register(byte address);
         Int32 Read20bitRegister(byte address);
