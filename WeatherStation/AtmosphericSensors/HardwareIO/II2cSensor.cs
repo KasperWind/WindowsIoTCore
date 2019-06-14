d87e1ab1df9ee81c14fbd004ce0742bdbcdc0163
Kasper Wind <Kasper@Wind.as>
2019-06-14 21:30:00 +0200

    Refactored I2c Sensor Interface

--- WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
+++ WeatherStation/AtmosphericSensors/HardwareIO/II2cSensor.cs
@@ -8,7 +8,6 @@ namespace AtmosphericSensors.HardwareIO
         UInt16 ReadUInt16Register(byte address);
         Int16 ReadInt16Register(byte address);
         Int32 Read20bitRegister(byte address);
-
         void WriteRegister(byte address, byte[] data);
     }
 }
