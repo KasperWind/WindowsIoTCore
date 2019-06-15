57f8b72c0ec7765c31939da4a947710bdc07633d
Kasper Wind <Kasper@Wind.as>
2019-06-15 10:20:14 +0200

    Refactored main page for compensation interface changes

--- WeatherStation/WeatherStation/MainPage.xaml.cs
+++ WeatherStation/WeatherStation/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using AtmosphericSensors.HardwareIO;
+using AtmosphericSensors.BME280;
 using AtmosphericSensors;
 using System.Diagnostics;
 using System.Timers;
@@ -38,8 +39,9 @@ namespace WeatherStation
 
         }
 
-        I2cSensor sensor = null;
-        AtmosphericSensors.BME280.BME280 tester = null;
+        private I2cSensor sensor = null;
+        private BME280 tester = null;
+        private BME280Compensations compensations = null;
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
@@ -62,9 +64,13 @@ namespace WeatherStation
                 {
                     sensor = new I2cSensor(0x77);
                 }
+                if (compensations == null)
+                {
+                    compensations = new BME280Compensations(sensor);
+                }
                 if (tester == null)
                 {
-                    tester = new AtmosphericSensors.BME280.BME280(sensor);
+                    tester = new BME280(sensor, compensations);
                 }
                 Debug.WriteLine("Temperature: {0:F3}°C; Humidity: {1:F3}%rH; Pressure: {2:F3}hPa", tester.GetTemperature(), tester.GetHumidity(), tester.GetBarometricPressure());
             }
